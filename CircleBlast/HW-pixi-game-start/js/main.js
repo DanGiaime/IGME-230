1aa4254b34d970877b8c3d97b663f9dfd11bdfbd
DanGiaime <dsg6874@g.rit.edu>
2017-11-28 23:59:46 -0500

    Circle 3

--- CircleBlast/HW-pixi-game-start/js/main.js
+++ CircleBlast/HW-pixi-game-start/js/main.js
@@ -174,34 +174,6 @@ function gameLoop(){
   ship.y = clamp (newY, 0+h2, sceneHeight-h2);
 
 	// #3 - Move Circles
-  class Circle extends PIXI.Graphics{
-    constructor(radius, color=0xFF0000, x=0, y=0){
-      super();
-      this.beginFill(color);
-      this.drawCircle(0,0,radius);
-      this.endFill();
-      this.x = x;
-      this.y = y;
-      this.radius = radius; // variables
-      this.fwd = getRandomUnitVector();
-      this.speed = 50;
-      this.isAlive = true;
-    }
-
-    move(dt=1/60) {
-      this.x += this.fwd.x * this.speed * dt;
-      this.y += this.fwd.y * this.speed * dt;
-    }
-
-    reflectX() {
-      this.fwd.x * -1;
-    }
-
-    reflectY() {
-      this.fwd.y * -1;
-    }
-  }
-
 
 	// #4 - Move Bullets
   createCircles(100);
